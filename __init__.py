06c07b029c6381fb00fc27798ff2fd46190268a0
James Ravenscroft <james@funkymonkeysoftware.com>
2011-04-14 00:19:08 +0100

    Finished basic listening server, now to add some useful behaviour

--- __init__.py
+++ __init__.py
@@ -4,3 +4,24 @@ Distcraft aims to provide a platform that can be used to set up distributed
 online games.
 '''
 
+import sys
+import optparse
+
+
+from core import DistCore
+
+DEFAULT_PORT = 1221
+
+if __name__ == "__main__":
+    
+    d = DistCore()
+    
+    if(len(sys.argv) > 1):
+        #get the options
+        o   = optparse.OptionParser()
+        o.add_option("-v", "--verbose", action="store_true", dest="verbose")
+        o.add_option("-d", "--debug", action="store_true", dest="debug")
+        o.add_option("-p", "--port" , action="store", dest="port", default=DEFAULT_PORT)
+        
+    else:
+        d.listen(DEFAULT_PORT)
--- core.py
+++ core.py
@@ -1,38 +1,89 @@
 import socket
+import threading
+    
+    
 
-
-class DistCore:
+class DistCore(object):
     
     __doc__= '''
     The DistCore class creates a listening server that is able to detect other 
     distcore requests and handle them
     '''
-    running = False
+    __running = False
     
     '''Create an empty dict ready for event handlers. These are registered
     with the core and fired by subprocesses'''
-    evt_handlers = {}
+    _evt_handlers = {}
     
     def __init__(self):
         #build an incoming socket for clients to join up with
         self.insock = socket.socket()
-        pass
         
+        #register internal core events
+        self.register_event_handler("distcraft.server.new_client", 
+                                        self._onAcceptClient)
+                                        
+    def __del__(self):
+        self.insock.close()
         
     def listen(self, port):
-        self.insock.bind( ("127.0.0.1", port))
+        '''Bind to a local socket and listen for incoming requests from users'''
+        self.insock.bind( ('127.0.0.1', port) )
+        
+        #listen for incoming connections
+        self.insock.listen(5)
         
         #wait for some incoming connections
-        running = True
+        self.__running = True
         
-        while(running == True):
+        while(self.__running == True):
             newsock, address = self.insock.accept()
+            self.fireEvent("distcraft.server.new_client", newsock, address)
             
     def register_event_handler(self, event, callback_function):
         '''Add an event handler with named callback function to the core'''
-        self.evt_handlers[event] = callback_function
+        self._evt_handlers[event] = callback_function
         
     def unregister_event_handler(self, event):
         '''Remove a registered event handler from the core'''
-        self.evt_handlers.pop(event)
+        self._evt_handlers.pop(event)
         
+    def fireEvent(self, event, *args):
+        '''Fires an event for the given event name or causes an error'''
+        if(self._evt_handlers.has_key(event)):
+            self._evt_handlers[event](*args)
+        else:
+            self.log("ERROR", "No event handler registered for %s" % event)
+            
+    def log(self, level, message):
+        print "[ %s ] %s]" % (level,message)
+        
+    def _onAcceptClient(self, clientsocket, clientaddress):
+        '''Accept an incoming connection from a client '''
+        c = CoreListenWorker(clientsocket, clientaddress)
+        
+        c.start()
+        
+
+class EventHandlerException(Exception):
+    '''Exception thrown when the event handler has problems'''
+    def __init__(self, message,eventname):
+        Exception.__init__("Error while handling event '%s', %s" % 
+                                                    (message,eventname))
+        
+class CoreListenWorker(threading.Thread):
+    '''This class is used to handle incoming client requests'''
+    
+    def __init__(self, clientsocket, clientaddress):
+        #initialse thread around this class
+        threading.Thread.__init__(self)
+        #store client socket and address
+        self.socket = clientsocket
+        self.addr = clientaddress
+        
+    def run(self):
+        '''Thread allows the worker to run and talk to the client'''
+        
+        #send the user hello message and wait for a response
+        self.socket.send("<message>hello</message>")
+        self.socket.close()
